449936b73a8a06f7387bcb5370b0982051cf2fe2
Andrey-Afoniy <mr.knop2015@yandex.ru>
2025-07-02 18:19:02 +0400

    HW 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public class Main {
+        public static void main(String[] args) {
+            Author author1 = new Author("Лев", "Толстой");
+            Author author2 = new Author("Джордж ", "Оруэлл");
+            Book book1 = new Book("Война и Мир", author1, 1998);
+            Book book2 = new Book("1984", author2, 1949);
+            book1.setPublishingYear(2015);
+            System.out.println("Книга: " + book1.getBookTitle() + ", автор: " + author1.getAuthorName() + " " + author1.getAuthorSurname() + ", год издания: " + book1.getPublishingYear());
+            System.out.println("Книга: " + book2.getBookTitle() + ", автор: " + author2.getAuthorName() + " " + author2.getAuthorSurname() + ", год издания: " + book2.getPublishingYear());
+
+        }
     }
-}
